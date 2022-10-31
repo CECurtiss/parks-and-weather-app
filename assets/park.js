520f65a99c3f55d3b445472ef07b25cf7838376f
CECurtiss <curtissclinton@gmail.com>
2022-10-31 12:52:03 -0500

    added vars and API pull to parkjs

--- assets/park.js
+++ assets/park.js
@@ -1,14 +1,24 @@
-function getparkcode() {
-Storage.getitem(parkcode)
-    var parkcodesearch = storage.getitem(parkcode)
-    var parkcodeUrl = ('https://developer.nps.gov/api/v1/parks?parkCode=' + parkCodeSearch + '&api_key=PrEprSmQZXCjkYEUgxFRqh1Ybv6fKiFDJIqmU1Iy')
+var longDescript = document.getElementById('longdescript');
+var parkCost = document.getElementById('parkcost');
+var parkHours = document.getElementById('parkhours');
+var petsDecript = document.getElementById('petdescription');
+var generalWeather = document.getElementById('generalweather');
+var parkContact = document.getElementById('parkcontact');
 
-    fetch(parkcodeUrl)
-
+function getParkCode() {
+    var parkCodeSearch = JSON.parse(localstorage.getItem('parkcode'));
+    console.log(parkCodeSearch);
+    var parkCodeUrl = ('https://developer.nps.gov/api/v1/parks?parkCode=' + parkCodeSearch + '&api_key=PrEprSmQZXCjkYEUgxFRqh1Ybv6fKiFDJIqmU1Iy')
+    fetch(parkCodeUrl)
     .then (function(response) {
-        return respomse.json() ;
+        return response.json() ;
     })
-    .then (function(data) {
-        console.log(data);
-    }
+    .then (function(response) {
+        console.log(response);
+        longDescript.textContent = response.data.description;
+        parkCost.textContent = response.data.entranceFees.description;
+        parkHours.textContent = response.data.operatingHours.standardHours;
+        generalWeather.textContent = response.data.weatherInfo;
+        parkContact.textContent = response.data.contacts.emailAddresses.emailAddress;
+    })
 }
