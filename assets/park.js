7a7da3e8cb26fce68b1fe151d842085da1eb780b
CECurtiss <curtissclinton@gmail.com>
2022-10-31 18:31:15 -0500

    forgot to save changes

--- assets/park.js
+++ assets/park.js
@@ -4,6 +4,7 @@ var parkHours = document.getElementById('parkhours');
 var petsDecript = document.getElementById('petdescription');
 var generalWeather = document.getElementById('generalweather');
 var parkContact = document.getElementById('parkcontact');
+var parkContainer = document.getElementsByClassName('container');
 
 function getParkCode() {
     var parkCodeSearch = localStorage.getItem('parkcode');
@@ -13,13 +14,24 @@ function getParkCode() {
         .then (function(response) {
         return response.json() ;
         })
-        .then (function(response) {
+        .then (function(response) {f
             console.log(response);
-            longDescript.textContent = response.data.description;
-            parkCost.textContent = response.data.entranceFees[0].description;
-            parkHours.textContent = response.data.operatingHours.standardHours;
-            generalWeather.textContent = response.data.weatherInfo;
-            parkContact.textContent = response.data.contacts.emailAddresses.emailAddress;
+            longDescript.textContent = response.data[0].description;
+            parkCost.textContent = response.data[0].entranceFees[0].description;
+            var hoursObject = response.data[0].operatingHours[0].standardHours;
+            for (key in hoursObject) {
+                var dayEl = document.createElement('p');
+                dayEl.textContent = key + " " + hoursObject[key];
+                parkHours.appendChild(dayEl);
+            }
+           
+            generalWeather.textContent = response.data[0].weatherInfo;
+            parkContact.textContent = response.data[0].contacts.emailAddresses[0].emailAddress;
+            var imgEl = document.createElement('img');
+            imgEl.src= "response.data[0].images[Math.floor(Math.random()* (response.data.images.length)]"
+            // imgEl.setAttribute("src", "response.data.images[Math.floor(Math.random()* (response.data.images.length)]");
+            parkContainer.appendChild(imgEl);
+            console.log(parkHours.textContent);
         })
     }
     getParkInfo();
